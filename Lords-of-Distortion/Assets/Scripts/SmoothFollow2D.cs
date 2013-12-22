c14b422ba463f7f35f6efd6d7d86ed9e1cc695ed
rmontes1 <rmontes1@ucsc.edu>
2013-12-22 14:57:59 -0800

    2D Controller
    2Dcontroller, sprites, basic level, basic camera if needed

---
+++ Lords-of-Distortion/Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2D : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime;
+	private Transform thisTransform;
+	private Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+		thisTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = new Vector3( Mathf.SmoothDamp( thisTransform.position.x, target.position.x, ref velocity.x, smoothTime),
+		                            Mathf.SmoothDamp( thisTransform.position.y, target.position.y, ref velocity.y, smoothTime),
+		                             thisTransform.position.z);
+
+		thisTransform.position = newPos;
+
+	}
+}
+
+
