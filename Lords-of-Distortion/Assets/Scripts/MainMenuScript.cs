c6fe01901ad0dd76a61df9d97e9ae7bf364222a6
jeala <jeala@ucsc.edu>
2013-12-27 20:37:38 -0800

    Main Menu and Options
    Very simple. Press start to go into game, press options to go to
    options.

---
+++ Lords-of-Distortion/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MainMenuScript : MonoBehaviour
+{
+    void OnGUI()
+    {
+        const int buttonWidth = 84;
+        const int buttonHeight = 60;
+
+        // Draw a button to start the game
+        if (
+          GUI.Button(
+            // Center in X, 2/3 of the height in Y
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (2 * Screen.height / 3) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Start!"
+          )
+        )
+        {
+            // On Click, load the first level.
+            // "Stage1" is the name of the first scene we created.
+            Application.LoadLevel("prototype");
+        }
+
+        // Draw a button to load options
+        if (
+          GUI.Button(
+            // Center in X, 5/6 of the height in Y
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (5 * Screen.height / 6) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Options"
+          )
+        )
+        {
+            // On Click, load the first level.
+            // "Stage1" is the name of the first scene we created.
+            Application.LoadLevel("Options");
+        }
+    }
+}
---
+++ Lords-of-Distortion/Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsScript : MonoBehaviour 
+{
+
+    public float allVolumeSlider = 100.0f;
+    public float bgmSlider = 100.0f;
+    public float sfxSlider = 100.0f;
+    const int buttonWidth = 84;
+    const int buttonHeight = 60;
+
+    void OnGUI()
+    {
+
+        // Optiosn Box. Contains all other boxes
+        GUI.Box(new Rect(10, 10, Screen.width - 20, Screen.height - 20), "Options");
+
+        // All Volume Box
+        GUI.Box(new Rect(50, (50 + Screen.height / 100), Screen.width - 100, 80), "All Volume");
+
+        // All Volume Slider
+        allVolumeSlider = GUI.HorizontalSlider(new Rect(75, 100 + Screen.height / 100, Screen.width - 160, 30), allVolumeSlider, 0.0f, 100.0f);
+
+        // BGM Box
+        GUI.Box(new Rect(50, 150 + Screen.height / 100, Screen.width - 100, 80), "BGM");
+
+        // BGM Slider
+        bgmSlider = GUI.HorizontalSlider(new Rect(75, 200 + Screen.height / 100, Screen.width - 160, 30), bgmSlider, 0.0f, 100.0f);
+
+        // SFX Box
+        GUI.Box(new Rect(50, 250 + Screen.height / 100, Screen.width - 100, 80), "SFX");
+
+        // SFX Slider
+        sfxSlider = GUI.HorizontalSlider(new Rect(75, 300 + Screen.height / 100, Screen.width - 160, 30), sfxSlider, 0.0f, 100.0f);
+
+        if (
+            GUI.Button(
+            // Center in X, 2/3 of the height in Y
+                new Rect(
+                    Screen.width / 2 - (buttonWidth / 2),
+                    (5 * Screen.height / 6) - (buttonHeight / 2),
+                    buttonWidth,
+                    buttonHeight
+                ),
+                "Back"
+            )
+        )
+        {
+            // On Click, load the first level.
+            // "Stage1" is the name of the first scene we created.
+            Application.LoadLevel("MainMenu");
+        }
+    }
+
+}
