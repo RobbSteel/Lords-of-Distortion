146d135a39643092f6bb4abe795fb6531f045b65
jeala <jeala@ucsc.edu>
2014-01-15 17:57:53 -0800

    Edited GravityField Prefab
    Gave it a GravityFieldTag

--- Lords-of-Distortion/Assets/Scripts/GravityField.cs
+++ Lords-of-Distortion/Assets/Scripts/GravityField.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class GravityField : MonoBehaviour {
 
+    private GameObject gravField;
+
 	// Update is called once per frame
 	void Update () {
-        GravField();
+        spawnGravField();
 	}
 
-    public void GravField()
+    public void spawnGravField()
     {
         
         if (Input.GetKeyDown(KeyCode.G) == true)
         {
             // Local Spawn
-            GameObject gravField = (GameObject)Instantiate(Resources.Load("gravityField"));
+            gravField = (GameObject)Instantiate(Resources.Load("gravityField"));
             Destroy(gravField, 5f);
             
             // Network Spawn - Not sure if this works, fails because "We are not connected"
