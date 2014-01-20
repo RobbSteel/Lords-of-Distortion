198f5a1e77d61177a36ea4d97d5a3f76f1c40120
jeala <jeala@ucsc.edu>
2014-01-19 17:08:28 -0800

    Added Timer Countdown for UI
    Saved in UI prefab as well

---
+++ Lords-of-Distortion/Assets/countdown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class countdown : MonoBehaviour {
+
+    public UILabel myLabel;
+
+    public float myTimer = 120.0f;
+
+	// Update is called once per frame
+    
+    void Update()
+    {
+        if (myTimer > 0)
+        {
+            myTimer -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(myTimer / 60F);
+            int seconds = Mathf.FloorToInt(myTimer - minutes * 60);
+
+            string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+            myLabel.text = niceTime;
+            //Application.LoadLevel(1);
+        }
+
+    }
+}
