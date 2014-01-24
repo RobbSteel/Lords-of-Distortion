e4c8930de71584b30769dd0b8f7328735028be67
RobbSteel <rsteel@ucsc.edu>
2014-01-24 02:01:14 -0800

    Scrolling Lobbies/Player Name
    This update contains refreshing hosts which will display available
    servers on a scroll bar. You can drag the server but clicking the server
    will crash it out and is next on the implementation list.

---
+++ Lords-of-Distortion/Assets/Scripts/JoinServer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinServer : MonoBehaviour {
+
+	public int servernumber;
+	public HostData[] hostList;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnClick(){
+
+		print (hostList);
+		Network.Connect(hostList[servernumber]);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Lords-of-Distortion/Assets/Scripts/PopulateMatches.cs
+++ Lords-of-Distortion/Assets/Scripts/PopulateMatches.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class PopulateMatches : MonoBehaviour {
 
-	public UILabel playerlabel;
+
 	public GameObject information;
-	public UILabel playertitle;
+	public GameObject playertitle;
+	public GameObject serverlabel;
 	public PSinfo infoscript;
-	///public Object labeled;
+
 	// Use this for initialization
 	void Start () {
-	  // playertitle = UILabel.Instantiate(playerlabel, transform.position, transform.rotation);
-		//playertitle = Instantiate(playerlabel, transform.position, transform.rotation);
+		playertitle = GameObject.Find("PlayerName");
 		information = GameObject.Find("PSInfo");
 		infoscript = information.GetComponent<PSinfo>();
-		//playertitle.text = infoscript.playername;
+		UILabel playerlabel = playertitle.GetComponentInChildren<UILabel>();
+		playerlabel.text = infoscript.playername;
+
+
+
+
+
 	}
 	
 	// Update is called once per frame
