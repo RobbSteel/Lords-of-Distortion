c21354d051f15346a02e51de1ac232f1fe0d6756
RobbSteel <rsteel@ucsc.edu>
2014-01-23 17:08:47 -0800

    UI For lobbies/hosting
    This contains some buttons and scene switching along with data
    transitions between scenes.

---
+++ Lords-of-Distortion/Assets/Scripts/PopulateMatches.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopulateMatches : MonoBehaviour {
+
+	public UILabel playerlabel;
+	public GameObject information;
+	public UILabel playertitle;
+	public PSinfo infoscript;
+	///public Object labeled;
+	// Use this for initialization
+	void Start () {
+	  // playertitle = UILabel.Instantiate(playerlabel, transform.position, transform.rotation);
+		//playertitle = Instantiate(playerlabel, transform.position, transform.rotation);
+		information = GameObject.Find("PSInfo");
+		infoscript = information.GetComponent<PSinfo>();
+		//playertitle.text = infoscript.playername;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		print(infoscript.playername);
+
+	}
+}
