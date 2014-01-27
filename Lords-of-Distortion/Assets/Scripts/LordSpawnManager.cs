b46cf5657543a6eaa44e4cc8948ef1711be80cf2
RobbSteel <rsteel@ucsc.edu>
2014-01-27 01:18:49 -0800

    LordManagerScript
    A script that can be attached to an empty game object in the arena scene
    to give the ability for buttons to spawn powers. Will need to be worked
    on as new power prefabs/sprites are added.

---
+++ Lords-of-Distortion/Assets/Scripts/LordSpawnManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LordSpawnManager : MonoBehaviour{
+
+
+
+	void Start(){
+
+		UIEventListener.Get(GameObject.Find("FireballButton")).onClick += InitializePower;
+	
+	}
+
+
+	void InitializePower(GameObject button){
+		
+		//var UIButton 
+		//UIButton
+		
+		UILabel powerlabel = button.GetComponent<UILabel>();
+		var powertype = powerlabel.text;
+		print (powertype);
+		PowerSpawn spawn = new PowerSpawn();
+		
+		
+		if(powertype == "Fireball"){
+			spawn.type = PowerSpawn.PowerType.FIREBALL;
+			currpower = powerlist[(int)spawn.type];
+			powerset = true;
+			
+		}
+	}
+
+	void Update(){
+
+		if(Input.GetMouseButtonDown(0) && powerset){
+
+		var mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+		mousePos.z = 0;
+
+		Instantiate (currpower, mousePos, Quaternion.identity);
+		powerset = false;
+		}
+	}
+
+
+
+
+
+
+	public float spawnTime;
+	public Vector3 position;
+	public Vector3 direction;
+	public Transform currpower;
+	public Vector3 mousePos;
+	public Transform[] powerlist; 
+	public bool powerset = false;
+	public ArrayList powerinfo;
+
+
+
+}
