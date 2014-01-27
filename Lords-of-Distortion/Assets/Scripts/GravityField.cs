3140b13f35bb6603477481578d7bd533d7a74abd
jeala <jeala@ucsc.edu>
2014-01-26 17:12:02 -0800

    Updated Gravity Field to work with updated power super class

--- Lords-of-Distortion/Assets/Scripts/GravityField.cs
+++ Lords-of-Distortion/Assets/Scripts/GravityField.cs
@@ -5,37 +5,59 @@ public class GravityField : Power {
 
     private GameObject gravField;
     public float timer = 0;
+    public bool prepareSpawn = false;
+    public bool fieldDestroyed = true;
 
 	// Update is called once per frame
 	void Update () {
         GravField();
 	}
 
-    public override void PowerAction(GameObject player, Controller2D controller)
+    public override void PowerActionEnter(GameObject player, Controller2D controller)
     {
         player.rigidbody2D.gravityScale = -1;
     }
 
-    public override void OnLoseContact(GameObject player, Controller2D controller)
+    public override void PowerActionStay(GameObject player, Controller2D controller)
+    {
+
+    }
+
+    public override void PowerActionExit(GameObject player, Controller2D controller)
     {
         player.rigidbody2D.gravityScale = 1;
     }
 
+  
+
     public void GravField()
     {
-        if (Input.GetKeyDown(KeyCode.G) == true)
+        if (Input.GetKeyUp(KeyCode.G) == true && fieldDestroyed == true)
         {
+            prepareSpawn = true;    
+        }
+
+        if (prepareSpawn == true && Input.GetMouseButtonUp(0))
+        {
+            fieldDestroyed = false;
+            prepareSpawn = false;
+            Vector3 mouseClick = Input.mousePosition;
+            mouseClick = Camera.main.ScreenToWorldPoint(mouseClick);
+
             // Local Spawn
             //GameObject gravField = (GameObject)Instantiate(Resources.Load("gravityField"));
 
             timer = Time.time;
 
             // Network Spawn - Not sure if this works, fails because "We are not connected"
-            gravField = (GameObject)Network.Instantiate(Resources.Load("gravityField"), new Vector3(0, 0, 0), Quaternion.identity, -1);
+            gravField = (GameObject)Network.Instantiate(Resources.Load("gravityField"), mouseClick, Quaternion.identity, -1);
         }
 
         if (Time.time - timer > 5)
+        {
+            fieldDestroyed = true;
             Network.Destroy(gravField);
+        }
     }
 
     void OnDestroy()
