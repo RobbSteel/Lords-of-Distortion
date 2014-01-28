0235186f4517e41241432ab78fb3753280eb076e
RobbSteel <rsteel@ucsc.edu>
2014-01-27 16:26:07 -0800

    Fix on Destroy

--- Lords-of-Distortion/Assets/Scripts/LordSpawnManager.cs
+++ Lords-of-Distortion/Assets/Scripts/LordSpawnManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class LordSpawnManager : MonoBehaviour{
@@ -11,6 +12,8 @@ public class LordSpawnManager : MonoBehaviour{
 		UIEventListener.Get(GameObject.Find("GravityFieldButton")).onClick += InitializePower;
 		UIEventListener.Get(GameObject.Find("SmokeBombButton")).onClick += InitializePower;
 		UIEventListener.Get(GameObject.Find("StickyTrapButton")).onClick += InitializePower;
+		powerspawned = new List<GameObject>();
+	
 	}
 
 
@@ -65,8 +68,8 @@ public class LordSpawnManager : MonoBehaviour{
 		var mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
 		mousePos.z = 0;
 
-		var newpower = Instantiate (currpower, mousePos, Quaternion.identity);
-		powerspawned.Add (newpower);
+		var newpower = (GameObject)Instantiate (currpower, mousePos, Quaternion.identity);
+		powerspawned.Add(newpower);
 		powerset = false;
 		}
 	}
@@ -92,7 +95,7 @@ public class LordSpawnManager : MonoBehaviour{
 	public Transform[] powerlist; 
 	public bool powerset = false;
 	public ArrayList powerinfo;
-	public ArrayList powerspawned;
+	public List<GameObject> powerspawned;
 
 
 }
