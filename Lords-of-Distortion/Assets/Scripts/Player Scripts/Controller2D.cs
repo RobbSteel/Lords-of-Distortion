4635f7073bba94bdfee242a24cd9d5357947480c
rmontes1 <rmontes1@ucsc.edu>
2014-02-07 15:21:10 -0800

    When player is stun now must press what horizontal keys are mapped to to escape stun

--- Lords-of-Distortion/Assets/Scripts/Player Scripts/Controller2D.cs
+++ Lords-of-Distortion/Assets/Scripts/Player Scripts/Controller2D.cs
@@ -104,7 +104,6 @@ public class Controller2D : MonoBehaviour {
 
 			
 			float move = Input.GetAxis ( "Horizontal" );
-
 			anim.SetFloat("Speed", Mathf.Abs(move));
 			
 			rigidbody2D.velocity = new Vector2( move * maxSpeed, rigidbody2D.velocity.y );
