cfcf33ca880697f42fc802f605bbeca6753600e7
jeala <jeala@ucsc.edu>
2014-02-07 15:43:12 -0800

    PowerSlot Prefab contains a UISprite, PowerSlot, with a Label as a child, PowerButton.
    There is a TempArray script, the images are loaded in by dragging and dropping into the script. Theres sample code to change the sprite and label text, but this will likely have to be modified when generating multiple icons.

---
+++ Lords-of-Distortion/Assets/Scripts/Trap Scripts/tempArray.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class tempArray : MonoBehaviour {
+
+    //Make array~dictionary containing powers
+    //Power Type and Sprite
+
+    public Dictionary<PowerType, Sprite> icons;
+    public Sprite glue;
+    public Sprite smoke;
+    public Sprite ink;
+    public Sprite wind;
+    private UISprite test;
+    private UILabel testLabel;
+    // Use this for initialization
+	void Start () {
+        icons = new Dictionary<PowerType, Sprite>();
+        icons.Add(PowerType.STICKY, glue);
+        icons.Add(PowerType.SMOKE, smoke);
+        icons.Add(PowerType.FIREBALL, ink);
+        icons.Add(PowerType.GRAVITY, wind);
+
+        //We wont be able to use this when we add in multiple icons because they'll be labeled differently,
+        //but its a start for now.
+        
+        //Something like this will change the background image of the sprite
+        test = (UISprite) GameObject.Find("PowerSlot").GetComponent("UISprite");
+        test.spriteName = ink.name;
+        //Something like this will change the label text.
+        testLabel = (UILabel)GameObject.Find("PowerButton").GetComponent("UILabel");
+        testLabel.text = "Q";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
