f248f354ea03edf162a12ca83de4b9307386cb3a
RobbSteel <rsteel@ucsc.edu>
2014-02-08 18:03:43 -0800

    Attempt at RPC's for display of scores

--- Lords-of-Distortion/Assets/Scripts/ScoreDisplay.cs
+++ Lords-of-Distortion/Assets/Scripts/ScoreDisplay.cs
@@ -7,36 +7,81 @@ public class ScoreDisplay : MonoBehaviour {
 	public GameObject PlayerLabel;
 	SessionManager sessionManager;
 	public float timeleft = 5;
-
+	PSinfo infoscript;
 
 	// Use this for initialization
 	void Start () {
 
+
+
+	}
+
+	//On Level Loaded tries to spawn the labels of player scores.
+
+	void OnNetworkLoadedLevel(){
+
+
 		sessionManager = GameObject.FindWithTag ("SessionManager").GetComponent<SessionManager>();
-		var PSinfo = GameObject.Find("PSInfo");
-		var infoscript = PSinfo.GetComponent<PSinfo>();
+		infoscript = sessionManager.gameInfo;
+		
+		
+		//Goes into server loop if hosting and else loop if not at the moment
+		
+		
+		
+		if(Network.isServer){
+			print("well then");
+			networkView.RPC("DisplayScores", RPCMode.Others);
+		} else {
+			
+			networkView.RPC ("NotifyDisplayScores", RPCMode.Server);
+			ShowScoresLocally();
+		}
 
-		var scorelabel = (GameObject)Instantiate(ScoreLabel, new Vector2(0,0), transform.rotation);
-		var playerlabel = (GameObject)Instantiate(PlayerLabel, new Vector2(0,0), transform.rotation);
-
-		scorelabel.transform.parent = GameObject.Find("UI Root").transform;
-		playerlabel.transform.parent = GameObject.Find("UI Root").transform;
-		scorelabel.transform.localScale = new Vector3(1, 1, 1);
-		playerlabel.transform.localScale = new Vector3(1, 1, 1);
-		scorelabel.transform.localPosition = new Vector2(-100, 0+(100*infoscript.playernumb));
-		playerlabel.transform.localPosition = new Vector2(100, 0+(50*infoscript.playernumb));
-
-		var scoretext = scorelabel.GetComponent<UILabel>();
-		var playertext = playerlabel.GetComponent<UILabel>();
-		scoretext.text = ((infoscript.score).ToString()) + " points";
-		playertext.text = infoscript.playername;
 	}
 
 
+	//Displays the labels with score and player info
+	void ShowScoresLocally(){
+		print ("gettting local");
+			var scorelabel = (GameObject)Instantiate(ScoreLabel, new Vector2(0,0), transform.rotation);
+			var playerlabel = (GameObject)Instantiate(PlayerLabel, new Vector2(0,0), transform.rotation);
+			
+			scorelabel.transform.parent = GameObject.Find("UI Root").transform;
+			playerlabel.transform.parent = GameObject.Find("UI Root").transform;
+			scorelabel.transform.localScale = new Vector3(1, 1, 1);
+			playerlabel.transform.localScale = new Vector3(1, 1, 1);
+			scorelabel.transform.localPosition = new Vector2(-100, 0+(200*infoscript.playernumb));
+			playerlabel.transform.localPosition = new Vector2(100, 0+(200*infoscript.playernumb));
+			
+			print (infoscript.playernumb);
+
+			
+			var scoretext = scorelabel.GetComponent<UILabel>();
+			var playertext = playerlabel.GetComponent<UILabel>();
+			scoretext.text = ((infoscript.score).ToString()) + " points";
+			playertext.text = infoscript.playername;
+
+	
 
 
+	}
 
 
+	[RPC]
+	void NotifyDisplayScores(){
+
+		networkView.RPC("DisplayScores", RPCMode.Others);
+		ShowScoresLocally();
+	}
+
+	[RPC]
+	void DisplayScores(){
+		print ("MERRRP");
+		ShowScoresLocally();
+
+
+	}
 	
 
 	// Update is called once per frame
