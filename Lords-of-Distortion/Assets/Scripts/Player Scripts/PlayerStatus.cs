c723b13e63e3c1d3df3e55a9eb1d2dd2de07e324
RobbSteel <rsteel@ucsc.edu>
2014-02-19 17:32:27 -0800

    Blinking FX for melee indication

--- Lords-of-Distortion/Assets/Scripts/Player Scripts/PlayerStatus.cs
+++ Lords-of-Distortion/Assets/Scripts/Player Scripts/PlayerStatus.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class PlayerStatus : MonoBehaviour {
-
-
+	
+	
 	public float maxStun = 100f;
 	public float currentStunMeter = 50f;
 	public float regenAmount = 15f;
@@ -14,7 +14,7 @@ public class PlayerStatus : MonoBehaviour {
 	public float UIoffsetX;
 	public float UIoffsetY;
 	public float recoverRate;
-
+	
 	private Controller2D playerControl;		// Reference to the PlayerControl script.
 	private GameObject UI;					// Reference UI GUI
 	private UISlider stunBarUI;				// Reference UI slider values
@@ -23,16 +23,18 @@ public class PlayerStatus : MonoBehaviour {
 	private bool horizontalPressedDown;
 	private int horizontalMoveCheck;		//tracks horizontal current key
 	//setup references and create UI stunbar
-
+	
 	int hitCount = 0;
 	public GameObject hitMarks;
 	SpriteRenderer hitMarkSprites;
 	public Sprite firstMark, secondMark, thirdMark;
 	public GameObject punchParticles;
+	public GameObject shieldParticles;
 	private ParticleSystem punchEffect;
+	private ParticleSystem shield;
 	bool knockBackPending = false;
 	Vector2 sideForce;
-
+	
 	void Awake(){
 		recoverRate = 10f;
 		horizontalPressedUp = false;
@@ -44,13 +46,15 @@ public class PlayerStatus : MonoBehaviour {
 	*/
 		hitMarkSprites = hitMarks.GetComponent<SpriteRenderer>();
 		punchEffect = punchParticles.GetComponent<ParticleSystem>();
+		shield = shieldParticles.GetComponent<ParticleSystem>();
+		shield.enableEmission = false;
 		levelCamera = GameObject.Find("Main Camera").camera;
 	}
-
+	
 	void Start(){
 		hitMarkSprites.enabled = false;
 	}
-
+	
 	//this function acts as unitys input keydown and up for "Horizontal" input
 	//**unity does not have this functionality yet needed to do this way 
 	//**because if we change movement for keys it wont apply correctly 
@@ -82,32 +86,32 @@ public class PlayerStatus : MonoBehaviour {
 				horizontalPressedDown = false;
 			}
 		}
-
+		
 	}
-
+	
 	//Updates StunBar UI and tints color relative to danger
 	void UpdateHealthBar(){
 		stunBarUI.value = currentStunMeter/maxStun;
 		stunBarUI.foregroundWidget.color = Color.Lerp( Color.yellow , Color.red, stunBarUI.value  );
 	}
-
+	
 	//sets position of stunbar correctly on player
 	void UpdateStunBarPosition(){
 		Vector3 playersPos = transform.position;
 		Vector3 screenPos = levelCamera.WorldToScreenPoint( playersPos );
 		float screenHeight = Screen.height;
 		float screenWidth = Screen.width;
-
+		
 		screenPos.x -= (screenWidth / 2.0f);
 		screenPos.y -= (screenHeight / 2.0f);
-
+		
 		screenPos.x += UIoffsetX;
 		screenPos.y += UIoffsetY;
-
+		
 		stunBarUI.transform.localPosition = screenPos;
-
+		
 	}
-
+	
 	
 	// Update is called once per frame
 	void Update () {
@@ -116,12 +120,12 @@ public class PlayerStatus : MonoBehaviour {
 		CheckIfStunned();
 		//UpdateStunBarPosition();
 	}
-
+	
 	[RPC]
 	void NotifyDamageTaken(float dmgTaken){
 		ApplyDamage(dmgTaken);
 	}
-
+	
 	private void ApplyDamage(float dmgTaken){
 		if (!playerControl.stunned) {
 			currentStunMeter += dmgTaken;
@@ -129,7 +133,7 @@ public class PlayerStatus : MonoBehaviour {
 				Stun (); //this will only matter if called on the client which controls the player to be stunned
 		}
 	}
-
+	
 	
 	void FixedUpdate(){
 		//push the character sideways
@@ -142,7 +146,7 @@ public class PlayerStatus : MonoBehaviour {
 			rigidbody2D.AddForce(sideForce);
 		}
 	}
-
+	
 	Vector2 upForce = new Vector2(0f, 300f);
 	//Push the character up on this step
 	void BeginKnockBack(float flip){
@@ -150,73 +154,77 @@ public class PlayerStatus : MonoBehaviour {
 		sideForce = new Vector2(800f * flip, 100f);
 		knockBackPending = true;
 	}
-
-
+	
+	
 	[RPC]
 	void NotifyHit(bool fromLeftSide){
 		//ignore hits if the player is already knocked back
 		if(playerControl.knockedBack){
 			return;
 		}
-
+		
 		hitCount++;
 		//This tells all other players to visually play a hit effect. 
 		//Only the owner of the character does anything with physics or death.
 		networkView.RPC ("VisualHitIndicator", RPCMode.Others, hitCount);
 		VisualHitIndicator(hitCount);
-
+		
 		if(hitCount == 2){
 			//hitCount= 0;
 			float flip = 1f;
 			if(!fromLeftSide)
 				flip = -1f;
-
+			
 			BeginKnockBack(flip);
 		}
-
+		
 		else if(hitCount == 3){
 			playerControl.Die();
 		}
-
+		
 	}
-
-
+	
+	
 	public void ThirdMarkSound(){
 		//Wait a little and then play bell sound.
 	}
-
-
+	
+	
 	public void HitFeedback(){
 		audio.Play ();
 		punchEffect.Play();
 	}
-
-
+	
+	
 	[RPC]
 	void VisualHitIndicator(int hits){
-
+		
 		HitFeedback();
-
-		hitMarkSprites.enabled = true;
-
+		
+		//hitMarkSprites.enabled = true;
+		
 		switch(hits){
 		case 0:
-			hitMarkSprites.enabled = false;
+			
+			//hitMarkSprites.enabled = false;
 			break;
 		case 1:
-			hitMarkSprites.sprite = firstMark;
+			shield.enableEmission = true;
+			
+			//hitMarkSprites.sprite = firstMark;
 			break;
 		case 2:
-			hitMarkSprites.sprite = thirdMark; //use this ugly one for now.
+			shield.startColor = Color.red;
+			//hitMarkSprites.sprite = thirdMark; //use this ugly one for now.
 			ThirdMarkSound();
 			break;
 		case 3:
 			//hitMarkSprites.sprite = thirdMark;
 			break;
 		}
-
+		
 	}
-
+	
 	//allows other objects to appliy a specific amount of damage
 	public void TakeDamage( float dmgTaken ){
 		//Don't worry about lag compensation for now and just notify all other of the stun bar being raised.
@@ -224,12 +232,12 @@ public class PlayerStatus : MonoBehaviour {
 		networkView.RPC("NotifyDamageTaken", RPCMode.Others, dmgTaken);
 		ApplyDamage(dmgTaken);
 	}
-
+	
 	//This function tells the player that owns this character that he's been hit by you.
 	public void AddHit( bool fromLeftSide){
 		networkView.RPC ("NotifyHit", GetComponent<NetworkController>().theOwner, fromLeftSide);
 	}
-
+	
 	//checks if player is stun and then applies stunRecover
 	private void CheckIfStunned(){
 		if( playerControl.stunned == true ){
@@ -241,7 +249,7 @@ public class PlayerStatus : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	public void Stun(){
 		playerControl.stunned = true;
 		playerControl.anim.SetBool("stunned", true);
@@ -253,7 +261,7 @@ public class PlayerStatus : MonoBehaviour {
 		currentStunMeter = maxStun;
 		recoverRate = newRecoverRate;
 	}
-
+	
 	//Only runs if your stun bar is injured
 	//slowly regenerates your stun bar
 	void RegenBar(){
@@ -261,26 +269,26 @@ public class PlayerStatus : MonoBehaviour {
 			if( currentStunMeter > 0 ){
 				if( regenTimer > regenCooldown ){
 					regenTimer = 0;
-
+					
 					if( currentStunMeter - regenAmount <= 0 ){
 						currentStunMeter = 0;
 					}else
 						currentStunMeter -= regenAmount;
-					}
+				}
 				else{
 					regenTimer += Time.deltaTime;
 				}
 			}
 		}
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	//Ondestroy delete Stunbar Ui
 	void OnDestroy(){
 		Destroy( UI );
 	}
-
+	
 } 
