0e7a4ced93895796efd714200e0dcf3c0042377c
flameiguana <flameiguana@gmail.com>
2014-03-02 13:07:43 -0800

    Added resupply sound for fountain

--- Lords-of-Distortion/Assets/Scripts/TrapFountain.cs
+++ Lords-of-Distortion/Assets/Scripts/TrapFountain.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TrapFountain : MonoBehaviour {
 
+	bool used = false;
+
 	// Use this for initialization
 	void Start () {
 		Destroy(gameObject, 6f);
@@ -11,5 +13,14 @@ public class TrapFountain : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			if(!used){
+				used = true;
+				audio.Play();
+			}
+		}
 	}
 }
