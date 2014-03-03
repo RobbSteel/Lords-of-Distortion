12ec10aacce0a3068c27203c5805286d32eb80ca
rmontes1 <rmontes1@ucsc.edu>
2014-03-02 16:13:45 -0800

    Adding Freeze Trap

---
+++ Lords-of-Distortion/Assets/Scripts/Trap Scripts/FreezeTrap.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeTrap : Power {
+
+	private PlayerStatus applyDmg;
+	public float trapDuration;
+
+	void Awake(){
+
+	}
+	void Start(){
+		trapDuration = 6f;
+	}
+
+	void Update(){
+		Destroy(gameObject, trapDuration );
+
+	}
+	
+	public override void PowerActionEnter(GameObject player, Controller2D controller){
+		applyDmg = controller.GetComponent<PlayerStatus> ();
+		applyDmg.Frozen();
+
+	}
+	
+	public override void PowerActionStay(GameObject player, Controller2D controller){
+
+	}
+	
+	public override void PowerActionExit(GameObject player, Controller2D controller){
+
+	}
+	
+	void OnDestroy(){
+
+	}
+}
