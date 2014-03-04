5435c7e1e0ba799d00068e4a0fad56929f7c18aa
flameiguana <flameiguana@gmail.com>
2014-03-03 17:31:40 -0800

    Added particles to fountain, temp sprite

--- Lords-of-Distortion/Assets/Scripts/TrapFountain.cs
+++ Lords-of-Distortion/Assets/Scripts/TrapFountain.cs
@@ -1,28 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class TrapFountain : MonoBehaviour {
 
+	public GameObject fountainParticles;
+	public GameObject getPowerParticles;
+	ParticleSystem getPowerPSystem;
+	
 	bool used = false;
 	public PlacementUI placementUI;
 
+	void Awake(){
+	
+		fountainParticles = (GameObject)Instantiate(fountainParticles, transform.position, Quaternion.identity);
+		fountainParticles.GetComponent<ParticleSystem>().Play();
+
+		getPowerParticles = (GameObject)Instantiate(getPowerParticles, transform.position, Quaternion.identity);
+		getPowerPSystem = getPowerParticles.GetComponent<ParticleSystem>();
+	}
 	// Use this for initialization
 	void Start () {
 		Destroy(gameObject, 6f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
+
+	
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Player"){
 			if(!used){
 				placementUI.Resupply();
 				used = true;
 				audio.Play();
+				getPowerPSystem.Play();
 			}
 		}
+	}
+
+	void OnDisable(){
+		Destroy(fountainParticles);
+		Destroy (getPowerParticles);
 	}
 }
