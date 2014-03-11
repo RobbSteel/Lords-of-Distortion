081b272bb847a915036d7cf771b20b392f6e600d
RobbSteel <rsteel@ucsc.edu>
2014-03-11 12:07:06 -0700

    Metric Fix

--- Lords-of-Distortion/Assets/Scripts/Trap Scripts/FreezeTrap.cs
+++ Lords-of-Distortion/Assets/Scripts/Trap Scripts/FreezeTrap.cs
@@ -40,12 +40,13 @@ public class FreezeTrap : Power {
 		applyDmg = controller.GetComponent<PlayerStatus> ();
 		applyDmg.Frozen();
 
-		if(GameObject.Find("CollectData") != null){
-			GA.API.Design.NewEvent("Times Frozen", player.transform.position);
-		}
+
 
 		//to keep from duplicating the effect since we had 2 colliders 
 		if (!controller.transform.FindChild ("FrozenEffect(Clone)")) {
+			if(GameObject.Find("CollectData") != null){
+				GA.API.Design.NewEvent("Times Frozen", player.transform.position);
+			}
 			frozenEffect = (GameObject)Instantiate(Resources.Load ("FrozenEffect"),player.transform.position, Quaternion.identity);
 			frozenEffect.GetComponent<FrozenEffect> ().checkStun = controller;
 			frozenplayer = controller;
