40770a23851f0ecbd2921012a578a6b1835472d9
RobbSteel <rsteel@ucsc.edu>
2014-03-12 15:52:11 -0700

    Fireball shoots upward after gravity, last man standing mode started

---
+++ Lords-of-Distortion/Assets/Resources/FreezeFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeFollow : MonoBehaviour {
+
+	public GameObject followplayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(followplayer != null){
+		transform.position = Vector2.MoveTowards(transform.position, followplayer.transform.position, 10);
+
+		}
+	}
+}
