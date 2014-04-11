c86018b419abaa8f0dda1850ee0e0eda871c42bf
flameiguana <flameiguana@gmail.com>
2014-04-11 04:18:42 -0700

    Network controller interpolation based on fixed delay instead of available packets now.   Changed time calculations to use double instead of float. Extrapolation improved by synching velocity.

--- Lords-of-Distortion/Assets/Test.cs
+++ Lords-of-Distortion/Assets/Test.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Linq;
 public class Test : MonoBehaviour {
-	
+
+	/*
+	 * coroutine
 	IEnumerator YieldSign() {
 		print ("spawn yield symbol");
 		yield return new WaitForSeconds(2);
@@ -16,5 +18,16 @@ public class Test : MonoBehaviour {
 	void Start() {
 		StartCoroutine(YieldSign());
 		print ("disable button");
+	}
+*/
+
+	void Start(){
+		CircularBuffer<int> buffer = new CircularBuffer<int>(2);
+		buffer.Add(1);
+		buffer.Add(2);
+		buffer.Add(3);
+		foreach(int i in buffer.Reverse<int>()){
+			print (i);
+		}
 	}
 }
