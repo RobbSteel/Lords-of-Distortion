5fedc4eb306c82f7a307c168b201791b9874dd8c
jjing <jjing@ucsc.edu>
2014-04-14 17:58:04 -0700

    Black hole done.

---
+++ Lords-of-Distortion/Assets/Scripts/Trap Scripts/BlackHoleRadius.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleRadius : Power 
+{
+	//Transform blackhole;
+	bool entered = false;
+	Transform target;
+	GameObject player;
+	Controller2D controller;
+	public float BHForce;
+	void Start () 
+	{
+		Destroy(gameObject, 15f);
+		BHForce = 0.22f;
+	}
+	void Update(){
+	}
+	
+	void FixedUpdate(){
+
+		
+		if (entered) {
+			move2BH(BHForce);
+		}
+	}
+	
+	public override void PowerActionEnter (GameObject player, Controller2D controller)
+	{
+		entered = true;
+	}
+	public override void PowerActionStay (GameObject player, Controller2D controller)
+	{
+		entered = true;
+		
+	}
+	public override void PowerActionExit (GameObject player, Controller2D controller)
+	{
+		entered = false;
+	}
+	
+	//Moves the player to black hole center
+	void move2BH(float speed){
+		var distance = Vector2.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position);
+		Debug.Log (distance);
+		if (distance > 0.1) {
+			GameObject.FindGameObjectWithTag ("Player").transform.position = Vector2.MoveTowards (GameObject.FindGameObjectWithTag ("Player").transform.position, transform.position, BHForce/distance);
+		} else 
+			entered = false;
+	}
+}
