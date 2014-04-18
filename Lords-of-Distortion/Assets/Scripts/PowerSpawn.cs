88ec58c4792126c3d5b179287711b70884641ce2
jeala <jeala@ucsc.edu>
2014-04-18 16:39:40 -0700

    Put Charmark behind powers

--- Lords-of-Distortion/Assets/Scripts/PowerSpawn.cs
+++ Lords-of-Distortion/Assets/Scripts/PowerSpawn.cs
@@ -51,14 +51,14 @@ public static class PowerTypeExtensions{
 		powersActive.Add(PowerType.HOLE);
 		powersActive.Add(PowerType.EXPLOSIVE);
 		powersActive.Add(PowerType.BOULDER);
-		
+        powersActive.Add(PowerType.PLAGUE);
+
 		powersPassive = new List<PowerType>();
-		//powersPassive.Add(PowerType.GRAVITY);
+		powersPassive.Add(PowerType.GRAVITY);
 		powersPassive.Add(PowerType.EARTH);
-		//powersPassive.Add(PowerType.SMOKE);
-		//powersPassive.Add (PowerType.PLAGUE);
-		//powersPassive.Add(PowerType.FREEZE);
-		//powersPassive.Add(PowerType.GATE);
+		powersPassive.Add(PowerType.SMOKE);
+		powersPassive.Add(PowerType.FREEZE);
+		powersPassive.Add(PowerType.GATE);
 
 		psuedoPowers.Add(PowerType.MELEE);
 		psuedoPowers.Add(PowerType.SPIKES);
