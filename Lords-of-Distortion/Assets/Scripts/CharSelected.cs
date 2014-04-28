4763f296a08fa323891787cac99ea922475fdabf
jjing <jjing@ucsc.edu>
2014-04-27 19:59:47 -0700

    Char select updated

--- Lords-of-Distortion/Assets/Scripts/CharSelected.cs
+++ Lords-of-Distortion/Assets/Scripts/CharSelected.cs
@@ -2,15 +2,32 @@
 using System.Collections;
 
 public class CharSelected : MonoBehaviour {
-	
     public PlayerServerInfo infoscript;
     public int characterNum;
-    
+	UIButtonColor buttcolor;
+
+	void Awake(){
+		buttcolor = GetComponent<UIButtonColor>();
+	}
+	/*
+	void Update(){
+				if (infoscript.localOptions.character == (PlayerOptions.Character)characterNum) {
+						buttcolor.defaultColor = Color.green;
+						buttcolor.hover = Color.green;
+						//buttcolor.pressed = Color.green;
+				} else {
+						buttcolor.defaultColor = Color.white;
+						//buttcolor.hover = Color.white;
+						//buttcolor.pressed = Color.white;
+				}
+		}
+*/
 	void OnPress(bool isDown)
     {
-        if(isDown)
-			return;
 
+		if (isDown) {
+			return;
+		}
         infoscript.localOptions.character = (PlayerOptions.Character)characterNum;
-	}	
+	}
 }
