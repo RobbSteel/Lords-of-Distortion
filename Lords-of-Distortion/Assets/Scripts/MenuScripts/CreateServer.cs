cab8ee06d46ea1045a4679e2f6bb1637a70f99c2
jeala <jeala@ucsc.edu>
2014-05-07 17:11:17 -0700

    Changed Controls
    Improved Main Menu
    Added new scene for host settings
    Added options scene (not being used)

--- Lords-of-Distortion/Assets/Scripts/MenuScripts/CreateServer.cs
+++ Lords-of-Distortion/Assets/Scripts/MenuScripts/CreateServer.cs
@@ -6,23 +6,26 @@ public class CreateServer : MonoBehaviour {
 	//public GameObject mainscript;
 	public MainGui playerscript;
 	public PlayerServerInfo infoscript;
-
+    public GameObject playerName;
 
 	// Use this for initialization
 	void Start () {
-	
+        infoscript = GameObject.Find("PSInfo").GetComponent<PlayerServerInfo>();
+        playerName.GetComponent<UILabel>().text = infoscript.localOptions.username;
 	}
 
 	void OnPress(){
 
-		if(playerscript.playerName == "" || playerscript.playerName == "Player Name" || playerscript.gameName == "" || playerscript.gameName == "Server Name"){
-			print("Nope");
-		} else {
-			infoscript.localOptions.username = playerscript.playerName;
+		if(playerscript.gameName == "" || playerscript.gameName == "Server Name")
+        {
+			    print("Nope");
+		}
+        else
+        {
+			//infoscript.localOptions.username = playerscript.playerName;
 			infoscript.servername = playerscript.gameName;
 			infoscript.choice = "Host";
 			Application.LoadLevel("LobbyArena");
-			
 		}
 	}	
 }
