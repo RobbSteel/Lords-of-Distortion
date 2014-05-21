873915267570a21393795368d9302a43d4984baf
jeala <jeala@ucsc.edu>
2014-05-21 12:24:16 -0700

    Added in game quit

---
+++ Lords-of-Distortion/Assets/LeaveGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaveGame : MonoBehaviour {
+
+    void OnPress()
+    {
+        Network.Disconnect(200);
+		if(Network.isServer)
+			MasterServer.UnregisterHost();
+    }
+
+}
---
+++ Lords-of-Distortion/Assets/OpenMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMenu : MonoBehaviour {
+
+    private bool pauseActive = false;
+    public GameObject menuHolder;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseActive)
+            {
+                menuHolder.SetActive(true);
+                pauseActive = true;
+            }
+            else
+            { 
+                menuHolder.SetActive(false);
+                pauseActive = false;
+            }
+        }
+    }
+
+}
