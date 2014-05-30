06d8076507600f821fd23a517f3667c58ae0ce27
flameiguana <flameiguana@gmail.com>
2014-05-30 00:56:45 -0700

    Custom cursor with temporary art.

--- Lords-of-Distortion/Assets/Scripts/GameInput.cs
+++ Lords-of-Distortion/Assets/Scripts/GameInput.cs
@@ -13,7 +13,10 @@ public class GameInput : MonoBehaviour {
 	public bool usingGamePad = true;
 
 
-	public KeyCode JumpKey = KeyCode.Space;
+	//Use this instead of Input.mousePosition
+	public Vector2 cursorPosition;
+	public Texture2D CursorTexture;
+	float analogSensitity = 750f;
 
 	void Awake()
 	{
@@ -29,10 +32,50 @@ public class GameInput : MonoBehaviour {
 
 	void Start () {
 		InputManager.Setup();
+		UseCustomCursor(true); //TODO: call this in game instead of menu
+		analogSensitity = Screen.width *.90f;
 	}
 
 	void Update () {
 		InputManager.Update();
+		GamePadToCursor();
+	}
+
+	bool useCustomCursor = false;
+
+	public void UseCustomCursor(bool use)
+	{
+		Screen.showCursor = !use;
+		useCustomCursor = use;
+	}
+
+	private Vector2 oldMousePosition;
+	private Rect cursorRect = new Rect(0f, 0f, 32f, 32f);
+
+
+
+	void GamePadToCursor()
+	{
+		InputDevice device = InputManager.ActiveDevice;
+		cursorPosition.x += device.RightStickX.Value * analogSensitity * Time.deltaTime;
+		cursorPosition.y += -device.RightStickY.Value * analogSensitity * Time.deltaTime;
+	}
+
+	void OnGUI()
+	{
+		Vector2 mousePosition = Event.current.mousePosition;
+		//We moved our mouse. Ignore gamepad's cursor location and use mouse's
+		if(oldMousePosition != mousePosition)
+		{
+			cursorPosition = mousePosition;
+		}
+		oldMousePosition = mousePosition;
+
+		if(useCustomCursor)
+		{
+			cursorRect.position = new Vector2(cursorPosition.x - 16f, cursorPosition.y - 16f);
+			GUI.DrawTexture (cursorRect, CursorTexture);
+		}
 	}
 }
 
