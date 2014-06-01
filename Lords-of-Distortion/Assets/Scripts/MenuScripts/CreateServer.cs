6d0be3a74cceebd131c728d068a5033e92b93b26
jeala <jeala@ucsc.edu>
2014-06-01 14:52:00 -0700

    Removed the code for accessing server name from main menu when the server name hasn't been created yet.
    Added the ready icon image back to the atlas

--- Lords-of-Distortion/Assets/Scripts/MenuScripts/CreateServer.cs
+++ Lords-of-Distortion/Assets/Scripts/MenuScripts/CreateServer.cs
@@ -6,37 +6,34 @@ public class CreateServer : MonoBehaviour {
 	//public GameObject mainscript;
 	public MainGui playerscript;
     public GameObject playerName;
+    public UILabel serverName;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		playerName.GetComponent<UILabel>().text = PlayerServerInfo.instance.localOptions.username;
 	}
 
 	void OnPress(){
 
-		if(playerscript.gameName == "" || playerscript.gameName == "Server Name")
+        if (serverName.text != "" && serverName.text != "Server Name")
         {
-			    print("Nope");
-		}
-        else
-        {
-			//infoscript.localOptions.username = playerscript.playerName;
-			PlayerServerInfo.instance.servername = playerscript.gameName;
-			PlayerServerInfo.instance.choice = "Host";
-            
-			var liveslabel = GameObject.Find("enterLives");
-			var lives = liveslabel.GetComponent<UILabel>().text;
-			var convert = float.Parse(lives);
-			if(convert == 0){
-				convert = 1;
-			}
+            PlayerServerInfo.instance.servername = serverName.text;
+            PlayerServerInfo.instance.choice = "Host";
+
+            var liveslabel = GameObject.Find("enterLives");
+            var lives = liveslabel.GetComponent<UILabel>().text;
+            var convert = float.Parse(lives);
+            if (convert == 0)
+            {
+                convert = 1;
+            }
 
             var numStages = GameObject.Find("enterRounds").GetComponent<UILabel>().text;
 
-			PlayerServerInfo.instance.lives = convert;
+            PlayerServerInfo.instance.lives = convert;
             PlayerServerInfo.instance.numStages = int.Parse(numStages);
-            //Debug.Log("NUMBER OF STAGES : " + PlayerServerInfo.instance.numStages);
-			Application.LoadLevel("LobbyArena");
-		}
+            Application.LoadLevel("LobbyArena");
+        }
 	}	
 }
