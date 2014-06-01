74efc1ac5e1cc2f5afd3481517fd6fb0d857b66e
jjing <jjing@ucsc.edu>
2014-05-31 17:46:53 -0700

    FindingGame button UI audio added
    MainMenu slight edit

--- Lords-of-Distortion/Assets/FlipLevel.cs
+++ Lords-of-Distortion/Assets/FlipLevel.cs
@@ -7,7 +7,8 @@ public class FlipLevel : MonoBehaviour {
 	private string LevelName = "MainMenu";
 	public bool flipleft = false;
 	public GameObject transitioner;
-	
+	public AudioClip buttonhover;
+	public AudioClip buttonclick;
 	
 	Transitioner fader;
 	
@@ -24,7 +25,13 @@ public class FlipLevel : MonoBehaviour {
 	{
 		if(!isDown)
 		{
+			audio.PlayOneShot(buttonclick);
 			fader.Flip(LevelName, flipleft);
+		}
+	}
+	void OnHover(bool isOver){
+		if (isOver) {
+			audio.PlayOneShot(buttonhover, 0.35f);
 		}
 	}
 }
