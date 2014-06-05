893b6070803dda2ea5d021b6ce67e7d55913ef17
jeala <jeala@ucsc.edu>
2014-06-05 01:03:22 -0700

    Added/Modified prefabs ready to be put in for StageSelect to support Bird Cage

---
+++ Lords-of-Distortion/Assets/SelectCageStages.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectCageStages : MonoBehaviour
+{
+    public GameObject PreviousSelection;
+    public Transitioner Transition;
+    public StageTracker trackmaster;
+    public UIPanel SelectUI;
+    public GameObject levelgrid;
+    public GameObject otherButton;
+    public bool turnBackwards;
+
+    [RPC]
+    void Flip()
+    {
+        PreviousSelection.SetActive(false);
+        levelgrid.SetActive(false);
+        SelectUI.alpha = 0;
+        trackmaster.Destroy();
+        Transition.Flip("None-Cage", turnBackwards);
+        if (otherButton != null)
+            otherButton.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    void OnClick()
+    {
+        if (Network.isServer)
+        {
+            PreviousSelection.SetActive(false);
+            levelgrid.SetActive(false);
+            SelectUI.alpha = 0;
+            trackmaster.Destroy();
+            Transition.Flip("None-Cage", turnBackwards);
+            networkView.RPC("Flip", RPCMode.Others);
+            if (otherButton != null)
+                otherButton.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
