c62217c55ad25316adfe6f84ae3ae63a137da6ce
rmontes1 <rmontes1@ucsc.edu>
2014-06-08 21:24:58 -0700

    Fixed Bat Particles having wrong texture , added birdParticle manager / birdParticles, wrote a script to make the birds be different random color pushing trapfounctain prefab with new trapfountain

---
+++ Lords-of-Distortion/Assets/ParticleFX/BirdParticles/Scripts/randomColorBirds.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomColorBirds : MonoBehaviour {
+
+
+	public bool randomColor;
+	public bool randomSolidColor;
+	public float colorSpawnLength;
+	private float timer;
+	private ParticleSystem birdParticles;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		birdParticles = this.GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setBirdColor ();
+	}
+
+	Vector4 randomColorCreate(){
+		return new Vector4 (randomNumber (), randomNumber (), randomNumber (), randomNumber ());
+	}
+
+	Vector4 randomSolidColorCreate(){
+		return new Vector4 (randomNumber (), randomNumber (), randomNumber (), 1f );
+	}
+
+	float randomNumber(){
+		return Random.Range (0f, 1f);
+	}
+
+	void setBirdColor(){
+		timer += Time.deltaTime;
+
+		if( timer >= colorSpawnLength ){
+			timer = 0;
+
+			if( randomSolidColor )
+			birdParticles.startColor = randomSolidColorCreate();
+
+			if( randomColor  )
+			birdParticles.startColor = randomColorCreate();
+
+		}
+	
+	}
+}
