51554f2095e13dd791269d4dd7877be8cc8880d1
flameiguana <flameiguana@gmail.com>
2014-06-08 18:36:59 -0700

    Added difficult desert stage.

---
+++ Lords-of-Distortion/Assets/FireballSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballSpawner : MonoBehaviour {
+	
+	public GameObject StageFireball;
+
+	float frequency = 6f;
+	float timer = 0f;
+
+	void Update()
+	{
+		if(timer <= 0f)
+		{
+			Instantiate(StageFireball, transform.position, StageFireball.transform.rotation);
+			timer = frequency + timer; //add remaining time (negative or zero)
+		}
+		else 
+			timer -= Time.deltaTime;
+	}
+}
